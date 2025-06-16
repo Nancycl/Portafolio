a972254a0c948d25c35dae1386c65480aa08bbe0
Nancy Sánchez <nancyclsanchez@gmail.com>
2025-06-16 16:32:28 -0600

    Nuevo commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+//import vercel from "@astrojs/vercel/serverless";
 
 // If you are not going to use vercel analytics you can delete the analytics
 // If you want to remove vercel from your project you can do npm uninstall vercel
 export default defineConfig({
-  output: "server",
+  output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
